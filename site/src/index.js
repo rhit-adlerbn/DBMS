2fcc479cfc6bceb4ec8f0672750ce5be0292e1e4
apet2929 <realandrewpetersen@gmail.com>
2023-04-19 17:12:48 -0400

    Example SQL queries in site

--- site/src/index.js
+++ site/src/index.js
@@ -1,24 +1,57 @@
-import { Connection } from 'tedious';  
-    var config = {  
-        server: 'titan.csse.rose-hulman.edu',
-        authentication: {
-            type: 'default',
-            options: {
-                userName: 'petersa2', //update me
-                password: 'Password123'  //update me
-            }
-        },
-        options: {
-            // If you are on Microsoft Azure, you need encryption:
-            encrypt: true,
-            database: 'DemonBase'  //update me
-        }
-    };  
-    
-    var connection = new Connection(config);  
-    connection.on('connect', function(err) {  
-        // If no error, then good to proceed.
-        console.log("Connected");  
+const Connection = require('tedious').Connection
+const Request = require('tedious').Request
+var TYPES = require('tedious').TYPES;
+
+function executeStatement() {
+    let request = new Request("select * from Demon", function(err, rowCount) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(rowCount + ' rows');
+        // and we close the connection
+        connection.close()
+      }
     });
-    
-    connection.connect();
+
+    request.on('row', function(columns) {
+        let result = ""
+      columns.forEach(function(column) {
+        result += column.value + ", "
+      });
+      console.log(result)
+    });
+
+    connection.execSql(request);
+}
+
+var config = {  
+    server: 'titan.csse.rose-hulman.edu',
+    authentication: {
+        type: 'default',
+        options: {
+            userName: 'petersa2', //update me
+            password: 'Password123'  //update me
+        }
+    },
+    options: {
+        // If you are on Microsoft Azure, you need encryption:
+        encrypt: true,
+        database: 'DemonBase',  //update me
+        trustServerCertificate: true
+    }
+};  
+
+
+var connection = new Connection(config);
+
+// Setup event handler when the connection is established. 
+connection.on('connect', function(err) {
+  if(err) {
+    console.log('Error: ', err)
+  }
+  // If no error, then good to go...
+  executeStatement();
+});
+
+// Initialize the connection.
+connection.connect();
